d837526188806f4c603f4119b8559b6bf1681232
sergey <sergey_kakadey@mail.ru>
2022-01-18 13:47:16 +0300

    add Tasks

--- src/components/Tasks/Task/Task.jsx
+++ src/components/Tasks/Task/Task.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import Css from './Taks.module.css'
 
 const Task = (props) => {
     return (
-        <div>
-            <input type={'text'}/>
-            <button><img src={'https://free-png.ru/wp-content/uploads/2021/06/free-png.ru-39-450x450.png'}/></button>
-            <button><img src={'https://cdn-icons-png.flaticon.com/512/45/45706.png'}/></button>
-            <button><img src={'https://free-png.ru/wp-content/uploads/2021/06/free-png.ru-38-450x450.png'}/></button>
+        <div className={Css.container}>
+            <input className={Css.input} type={'text'}/>
+            <div className={Css.buttons}>
+                <button className={Css.button}><img className={Css.img} src={'https://free-png.ru/wp-content/uploads/2021/06/free-png.ru-39-450x450.png'}/></button>
+                <button className={Css.button}><img className={Css.img} src={'https://cdn-icons-png.flaticon.com/512/45/45706.png'}/></button>
+                <button className={Css.button}><img className={Css.img} src={'https://free-png.ru/wp-content/uploads/2021/06/free-png.ru-38-450x450.png'}/></button>
+            </div>
         </div>
     )
 }
--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Task from "./Task/Task";
+import Css from "./Tasks.module.css"
 
 const Tasks = (props) => {
     return (
-        <div>
+        <div className={Css.container}>
             <Task/>
             <Task/>
             <Task/>
