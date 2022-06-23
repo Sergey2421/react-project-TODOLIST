fa6aa11d9be193eee2ffe403c97f8ddf90a2a21d
sergey <sergey_kakadey@mail.ru>
2022-06-23 11:08:42 +0300

    fix: add alt atrb for imgs

--- src/components/Tasks/CompletedTask/CompletedTask.jsx
+++ src/components/Tasks/CompletedTask/CompletedTask.jsx
@@ -14,7 +14,7 @@ const CompletedTask = (props) => {
         <div className={Css.container}>
             <input disabled={true} className={Css.input} type={'text'} value={props.text}/>
             <div className={Css.buttons}>
-                <button className={Css.button} onClick={deleteCompletedTask}><img className={Css.img} src={'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Crystal_button_cancel.svg/32px-Crystal_button_cancel.svg.png?20080403113445'}/></button>
+                <button className={Css.button} onClick={deleteCompletedTask}><img alt={'Удалить'} className={Css.img} src={'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Crystal_button_cancel.svg/32px-Crystal_button_cancel.svg.png?20080403113445'}/></button>
             </div>
         </div>
     )
